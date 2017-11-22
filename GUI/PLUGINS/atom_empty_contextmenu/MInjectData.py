6d9549715d600675405ed44abfd2ba576314bd44
muyanru <suibian19881213>
2017-11-22 16:00:27 +0800

    use injectDataDialog

--- GUI/PLUGINS/atom_empty_contextmenu/MInjectData.py
+++ GUI/PLUGINS/atom_empty_contextmenu/MInjectData.py
@@ -8,9 +8,8 @@
 
 from GUI.PLUGINS.MPluginBase import MPluginBase
 from GUI.QT import *
-from GUI.WIDGETS.MInputDialog import MInputDialog
+from GUI.WIDGETS.MInjectDataDialog import MInjectDataDialog
 from GUI.PLUGINS.MTableHandle import *
-
 
 class MInjectData(MPluginBase):
     name = 'Inject Data'
@@ -20,19 +19,10 @@ class MInjectData(MPluginBase):
     def run(self, event):
         parentWidget = event.get('parentWidget')
         orm = event.get('orm')
-        result = True
-        name = 'NewDATA1'
-        while result:
-            name, result = MInputDialog.getText(parentWidget, 'New ATOM', 'Name:', name, MData._nameRegExp)
-            if result:
-                if MData.validateExist(name, orm):
-                    QMessageBox.critical(parentWidget, 'ERROR', 'This name exists.')
-                    continue
-                else:
-                    QMessageBox.information(parentWidget, 'INFO', 'Here is a new dialog for IO inject data')
-                    MData.inject(name=name, atoms=[orm])
-                    self.emit(SIGNAL('sigRefresh()'))
-                    return
+        dialog = MInjectDataDialog(orm, parentWidget)
+        dialog.exec_()
+        self.emit(SIGNAL('sigRefresh()'))
+        return
 
     def validate(self, event):
         return True

56430d6673faa9a4fe291d04fc840e6267f25fa5
muyanru <suibian19881213>
2017-11-28 11:05:16 +0800

    add download action

---
+++ GUI/PLUGINS/atom_contextmenu/MDownloadAtom.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+###################################################################
+# Author: Mu yanru
+# Date  : 2017.7
+# Email : muyanru345@163.com
+###################################################################
+
+from GUI.PLUGINS.MPluginBase import MPluginBase
+from GUI.PLUGINS.MTableHandle import *
+from GUI.WIDGETS.MJobMonitor import MJobMonitor
+
+
+class MDownloadAtom(MPluginBase):
+    name = 'Download'
+    icon = 'icon-download.png'
+    needRefresh = True
+    shortcut = QKeySequence.Copy
+
+    def run(self, event):
+        ormList = event.get('orm')
+        parentWidget = event.get('parentWidget')
+        folder = QFileDialog.getExistingDirectory(parentWidget, 'Save Folder', '', )
+        if folder:
+            #TODO: add job to db
+            dialog = MJobMonitor(parentWidget)
+            if dialog.exec_():
+                QMessageBox.information(self, 'OK', 'SUCCESS')
+
+    def validate(self, event):
+        orm = event.get('orm')
+        if isinstance(orm, list) and len(orm) > 1:
+            return False
+        return True

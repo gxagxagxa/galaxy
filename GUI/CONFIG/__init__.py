dfb6b2ad073cbf63d300b40e57bf9e5b536ea84a
muyanru <suibian19881213>
2017-11-28 11:03:47 +0800

    add job monitor config

--- GUI/CONFIG/__init__.py
+++ GUI/CONFIG/__init__.py
@@ -5,3 +5,4 @@ def loadUIConfig(jsonFile):
     return json.loads(pkgutil.get_data('GUI.CONFIG', jsonFile))
 
 MInjectDataConfigFunc = partial(loadUIConfig, 'MInjectDataDialog.json')
+MJobMonitorConfigFunc = partial(loadUIConfig, 'MJobMonitor.json')
